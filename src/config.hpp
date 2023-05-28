e1aabac21c759258de54bcd7797c574bb9743267
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-05-28 11:36:32 +0800

    first commit

---
+++ src/config.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+/*Board Size, Don't change!*/
+#define BOARD_H 6
+#define BOARD_W 5
+
+/*You can modify these things in development for fast testing*/
+/*When TA run your program, we will use default settings (123, 10, 50)*/
+#define RANDOM_SEED 123
+#define timeout 10
+#define MAX_STEP 50
